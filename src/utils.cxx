8057f9a53fe4cb385920b1edc24c1d0bb91b4367
TheFGFSEagle <thefgfseagle@gmail.com>
2024-05-07 12:25:08 +0200

    Fix undefined references due to utility function implementation not being in easyqt namespace, upgrade singletonpp dependency

--- src/utils.cxx
+++ src/utils.cxx
@@ -1,21 +1,23 @@
 #include "utils.hxx"
 
-std::string getResourcePath(std::string path) {
-	QFile resolver(("res:" + path).c_str());
-	return resolver.fileName().toStdString();
-}
-
-std::string strftime(std::time_t time, const std::string& format) {
-	std::tm* local_time = std::localtime(&time);
-	char output[256];
-	std::strftime(output, sizeof(output), format.c_str(), local_time);
-	return output;
-}
-std::string strftime(std::time_t time, const char* format) {
-	std::tm* local_time = std::localtime(&time);
-	char output[256];
-	std::strftime(output, sizeof(output), format, local_time);
-	return output;
+namespace easyqt {
+	std::string getResourcePath(std::string path) {
+		QFile resolver(("res:" + path).c_str());
+		return resolver.fileName().toStdString();
+	}
+	
+	std::string strftime(std::time_t time, const std::string& format) {
+		std::tm* local_time = std::localtime(&time);
+		char output[256];
+		std::strftime(output, sizeof(output), format.c_str(), local_time);
+		return output;
+	}
+	std::string strftime(std::time_t time, const char* format) {
+		std::tm* local_time = std::localtime(&time);
+		char output[256];
+		std::strftime(output, sizeof(output), format, local_time);
+		return output;
+	}
 }
 
 template<typename T>
